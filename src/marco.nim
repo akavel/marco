79e5c58a2efedb4a0d51c363a96e0225d08b2035
Mateusz Czapliński <czapkofan@gmail.com>
2019-10-10 01:25:33 +0200

    add -i & -o flags

--- src/marco.nim
+++ src/marco.nim
@@ -1,9 +1,22 @@
 {.experimental: "codeReordering".}
+import parseopt
 import streams
 import xmlparser
 import marcopkg/compile
 
+var
+  input = stdin.newFileStream
+  output = stdout.newFileStream
+
+for kind, key, val in getopt():
+  case key
+  of "i": input = val.openFileStream
+  of "o": output = val.openFileStream
+  else:
+    stderr.write("unknown flag: " & key)
+    quit(1)
+
 # when isMainModule:
 
 # stdout.write(marcoCompile(parseXml(newFileStream(stdin))))
-stdout.write(stdin.newFileStream.parseXml.marcoCompile)
+output.write(input.parseXml.marcoCompile)
